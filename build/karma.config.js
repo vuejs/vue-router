3381c1b6408f7885ef359b128dc23471cdeb5cdc
Evan You <yyx990803@gmail.com>
2015-07-27 18:11:26 -0400

    add coverage

--- build/karma.config.js
+++ build/karma.config.js
@@ -1,15 +1,32 @@
 module.exports = function (config) {
   config.set({
-    browsers: ['Chrome', 'Firefox'],
-    reporters: ['progress'],
+    browsers: ['Chrome'],
+    reporters: ['spec', 'coverage'],
     frameworks: ['jasmine'],
     files: ['../test/test.js'],
     preprocessors: {
       '../test/test.js': ['webpack']
     },
+    webpack: {
+      module: {
+        postLoaders: [
+          {
+            test: /\.js$/,
+            exclude: /(test|node_modules|vue\/src)\//,
+            loader: 'istanbul-instrumenter'
+          }
+        ]
+      }
+    },
     webpackMiddleware: {
       noInfo: true
     },
-    singleRun: true
+    singleRun: true,
+    coverageReporter: {
+      reporters: [
+        { type: 'lcov', dir: '../coverage', subdir: '.' },
+        { type: 'text-summary', dir: '../coverage', subdir: '.' }
+      ]
+    }
   })
 }
