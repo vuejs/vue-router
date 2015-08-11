463c6ac8de482e2516696176d44ea477737f2716
Evan You <yyx990803@gmail.com>
2015-08-10 23:55:05 -0400

    use dev server hot mode

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  entry: './example/index.js',
+  entry: {
+    app: ['webpack/hot/dev-server', './example/index.js']
+  },
   output: {
     path: './example',
     filename: 'example.build.js'
