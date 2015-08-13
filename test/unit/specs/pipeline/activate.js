6cf5776cecabd7888365dbae3235d90ab4f366f9
Evan You <yyx990803@gmail.com>
2015-08-13 11:19:18 -0400

    full activate/deactivate tests

--- test/unit/specs/pipeline/activate.js
+++ test/unit/specs/pipeline/activate.js
@@ -56,4 +56,72 @@ describe('activate', function () {
       done()
     })
   })
+
+  it('abort async', function (done) {
+    test({
+      a: {
+        activate: function (transition) {
+          setTimeout(function () {
+            transition.abort()
+          }, wait)
+        }
+      }
+    }, function (router, calls, emitter) {
+      router.go('/a')
+      assertCalls(calls, ['a.activate'])
+      expect(router.app.$el.textContent).toBe('')
+      // path changes during validation phase
+      expect(router.history.currentPath).toBe('/a')
+      setTimeout(function () {
+        // but gets reset when validation fails
+        expect(router.app.$el.textContent).toBe('')
+        expect(router.history.currentPath).toBe('/')
+        done()
+      }, wait)
+    })
+  })
+
+  it('promise', function (done) {
+    test({
+      a: {
+        activate: function (transition) {
+          return new Promise(function (resolve) {
+            setTimeout(resolve, wait)
+          })
+        }
+      }
+    }, function (router, calls, emitter) {
+      router.go('/a')
+      expect(router.app.$el.textContent).toBe('')
+      setTimeout(function () {
+        assertCalls(calls, ['a.activate'])
+        expect(router.app.$el.textContent).toBe('A ')
+        done()
+      }, wait)
+    })
+  })
+
+  it('promise reject', function (done) {
+    test({
+      a: {
+        activate: function (transition) {
+          return new Promise(function (resolve, reject) {
+            setTimeout(reject, wait)
+          })
+        }
+      }
+    }, function (router, calls, emitter) {
+      router.go('/a')
+      assertCalls(calls, ['a.activate'])
+      expect(router.app.$el.textContent).toBe('')
+      // path changes during validation phase
+      expect(router.history.currentPath).toBe('/a')
+      setTimeout(function () {
+        // but gets reset when validation fails
+        expect(router.app.$el.textContent).toBe('')
+        expect(router.history.currentPath).toBe('/')
+        done()
+      }, wait)
+    })
+  })
 })
--- test/unit/specs/pipeline/deactivate.js
+++ test/unit/specs/pipeline/deactivate.js
@@ -61,4 +61,81 @@ describe('deactivate', function () {
       done()
     })
   })
+
+
+  it('abort async', function (done) {
+    test({
+      a: {
+        deactivate: function (transition) {
+          setTimeout(function () {
+            transition.abort()
+          }, wait)
+        }
+      }
+    }, function (router, calls, emitter) {
+      router.go('/a')
+      expect(router.app.$el.textContent).toBe('A ')
+      router.go('/b')
+      assertCalls(calls, ['a.deactivate'])
+      // content hasn't changed yet
+      expect(router.app.$el.textContent).toBe('A ')
+      // path is changed
+      expect(router.history.currentPath).toBe('/b')
+      setTimeout(function () {
+        // aborted, path is reset
+        expect(router.app.$el.textContent).toBe('A ')
+        expect(router.history.currentPath).toBe('/a')
+        done()
+      }, wait)
+    })
+  })
+
+  it('promise', function (done) {
+    test({
+      a: {
+        deactivate: function () {
+          return new Promise(function (resolve) {
+            setTimeout(resolve, wait)
+          })
+        }
+      }
+    }, function (router, calls, emitter) {
+      router.go('/a')
+      expect(router.app.$el.textContent).toBe('A ')
+      router.go('/b')
+      expect(router.app.$el.textContent).toBe('A ')
+      setTimeout(function () {
+        assertCalls(calls, ['a.deactivate'])
+        expect(router.app.$el.textContent).toBe('')
+        done()
+      }, wait)
+    })
+  })
+
+  it('promise reject', function (done) {
+    test({
+      a: {
+        deactivate: function () {
+          return new Promise(function (resolve, reject) {
+            setTimeout(reject, wait)
+          })
+        }
+      }
+    }, function (router, calls, emitter) {
+      router.go('/a')
+      expect(router.app.$el.textContent).toBe('A ')
+      router.go('/b')
+      assertCalls(calls, ['a.deactivate'])
+      // content hasn't changed yet
+      expect(router.app.$el.textContent).toBe('A ')
+      // path is changed
+      expect(router.history.currentPath).toBe('/b')
+      setTimeout(function () {
+        // aborted, path is reset
+        expect(router.app.$el.textContent).toBe('A ')
+        expect(router.history.currentPath).toBe('/a')
+        done()
+      }, wait)
+    })
+  })
 })
