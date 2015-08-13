6146342d734bc13397224da3dc31f6e1ef1a6369
Evan You <yyx990803@gmail.com>
2015-08-13 17:51:31 -0400

    include release script

---
+++ build/release.sh
@@ -0,0 +1,15 @@
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Releasing $VERSION ..."
+    git add -A
+    git commit -m "[build] $VERSION"
+    npm version $VERSION --message "[release] $VERSION"
+    git push origin refs/tags/v$VERSION
+    git push
+    npm publish
+fi
