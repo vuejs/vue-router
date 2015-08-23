1d24418b51025285caa92fc8d9ea553782a5d5e1
Evan You <yyx990803@gmail.com>
2015-08-23 16:31:59 -0400

    structure docs

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -1,8 +1,8 @@
 cd docs
 rm -rf _book
 gitbook build
-cp _circle.yml _book/circle.yml
-cp CNAME _book/CNAME
+cp assets/circle.yml _book/circle.yml
+cp assets/CNAME _book/CNAME
 cd _book
 git init
 git add -A
