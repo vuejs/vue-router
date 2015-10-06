d9912e835df5c9ff757648c54e790407ced4481d
Evan You <yyx990803@gmail.com>
2015-10-06 18:04:17 -0400

    move router functions into the same file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import util, { warn } from './util'
-import initAPI from './router/api'
-import initInternal from './router/internal'
+import util, { warn, mapParams } from './util'
 import initMixin from './mixin'
 import Recognizer from 'route-recognizer'
+import Route from './route'
+import Transition from './transition'
 import View from './directives/view'
 import Link from './directives/link'
 import AbstractHistory from './history/abstract'
@@ -14,6 +14,9 @@ const historyBackends = {
   hash: HashHistory,
   html5: HTML5History
 }
+
+// late bind during install
+let Vue
 
 /**
  * Router constructor
@@ -84,7 +87,7 @@ class Router {
     this._suppress = suppressTransitionError
 
     // create history object
-    let inBrowser = util.Vue.util.inBrowser
+    let inBrowser = Vue.util.inBrowser
     this.mode = (!inBrowser || this._abstract)
       ? 'abstract'
       : this._history
@@ -101,7 +104,439 @@ class Router {
       }
     })
   }
+
+  // API ===================================================
+
+    /**
+   * Register a map of top-level paths.
+   *
+   * @param {Object} map
+   */
+
+  map (map) {
+    for (let route in map) {
+      this.on(route, map[route])
+    }
+  }
+
+  /**
+   * Register a single root-level path
+   *
+   * @param {String} rootPath
+   * @param {Object} handler
+   *                 - {String} component
+   *                 - {Object} [subRoutes]
+   *                 - {Boolean} [forceRefresh]
+   *                 - {Function} [before]
+   *                 - {Function} [after]
+   */
+
+  on (rootPath, handler) {
+    if (rootPath === '*') {
+      this._notFound(handler)
+    } else {
+      this._addRoute(rootPath, handler, [])
+    }
+  }
+
+  /**
+   * Set redirects.
+   *
+   * @param {Object} map
+   */
+
+  redirect (map) {
+    for (let path in map) {
+      this._addRedirect(path, map[path])
+    }
+  }
+
+  /**
+   * Set aliases.
+   *
+   * @param {Object} map
+   */
+
+  alias (map) {
+    for (let path in map) {
+      this._addAlias(path, map[path])
+    }
+  }
+
+  /**
+   * Set global before hook.
+   *
+   * @param {Function} fn
+   */
+
+  beforeEach (fn) {
+    this._beforeEachHooks.push(fn)
+  }
+
+  /**
+   * Set global after hook.
+   *
+   * @param {Function} fn
+   */
+
+  afterEach (fn) {
+    this._afterEachHooks.push(fn)
+  }
+
+  /**
+   * Navigate to a given path.
+   * The path can be an object describing a named path in
+   * the format of { name: '...', params: {}, query: {}}
+   * The path is assumed to be already decoded, and will
+   * be resolved against root (if provided)
+   *
+   * @param {String|Object} path
+   * @param {Boolean} [replace]
+   */
+
+  go (path, replace) {
+    path = this._normalizePath(path)
+    this.history.go(path, replace)
+  }
+
+  /**
+   * Short hand for replacing current path
+   *
+   * @param {String} path
+   */
+
+  replace (path) {
+    this.go(path, true)
+  }
+
+  /**
+   * Start the router.
+   *
+   * @param {VueConstructor} App
+   * @param {String|Element} container
+   * @param {Function} [cb]
+   */
+
+  start (App, container, cb) {
+    /* istanbul ignore if */
+    if (this._started) {
+      warn('already started.')
+      return
+    }
+    this._started = true
+    this._startCb = cb
+    if (!this.app) {
+      /* istanbul ignore if */
+      if (!App || !container) {
+        throw new Error(
+          'Must start vue-router with a component and a ' +
+          'root container.'
+        )
+      }
+      this._appContainer = container
+      this._appConstructor = typeof App === 'function'
+        ? App
+        : Vue.extend(App)
+    }
+    this.history.start()
+  }
+
+  /**
+   * Stop listening to route changes.
+   */
+
+  stop () {
+    this.history.stop()
+    this._started = false
+  }
+
+  // Internal methods ======================================
+
+    /**
+   * Add a route containing a list of segments to the internal
+   * route recognizer. Will be called recursively to add all
+   * possible sub-routes.
+   *
+   * @param {String} path
+   * @param {Object} handler
+   * @param {Array} segments
+   */
+
+  _addRoute (path, handler, segments) {
+    guardComponent(handler)
+    segments.push({
+      path: path,
+      handler: handler
+    })
+    this._recognizer.add(segments, {
+      as: handler.name
+    })
+    // add sub routes
+    if (handler.subRoutes) {
+      for (let subPath in handler.subRoutes) {
+        // recursively walk all sub routes
+        this._addRoute(
+          subPath,
+          handler.subRoutes[subPath],
+          // pass a copy in recursion to avoid mutating
+          // across branches
+          segments.slice()
+        )
+      }
+    }
+  }
+
+  /**
+   * Set the notFound route handler.
+   *
+   * @param {Object} handler
+   */
+
+  _notFound (handler) {
+    guardComponent(handler)
+    this._notFoundHandler = [{ handler: handler }]
+  }
+
+  /**
+   * Add a redirect record.
+   *
+   * @param {String} path
+   * @param {String} redirectPath
+   */
+
+  _addRedirect (path, redirectPath) {
+    this._addGuard(path, redirectPath, this.replace)
+  }
+
+  /**
+   * Add an alias record.
+   *
+   * @param {String} path
+   * @param {String} aliasPath
+   */
+
+  _addAlias (path, aliasPath) {
+    this._addGuard(path, aliasPath, this._match)
+  }
+
+  /**
+   * Add a path guard.
+   *
+   * @param {String} path
+   * @param {String} mappedPath
+   * @param {Function} handler
+   */
+
+  _addGuard (path, mappedPath, handler) {
+    this._guardRecognizer.add([{
+      path: path,
+      handler: (match, query) => {
+        let realPath = mapParams(
+          mappedPath,
+          match.params,
+          query
+        )
+        handler.call(this, realPath)
+      }
+    }])
+  }
+
+  /**
+   * Check if a path matches any redirect records.
+   *
+   * @param {String} path
+   * @return {Boolean} - if true, will skip normal match.
+   */
+
+  _checkGuard (path) {
+    let matched = this._guardRecognizer.recognize(path)
+    if (matched) {
+      matched[0].handler(matched[0], matched.queryParams)
+      return true
+    }
+  }
+
+  /**
+   * Match a URL path and set the route context on vm,
+   * triggering view updates.
+   *
+   * @param {String} path
+   * @param {Object} [state]
+   * @param {String} [anchor]
+   */
+
+  _match (path, state, anchor) {
+    if (this._checkGuard(path)) {
+      return
+    }
+
+    let prevRoute = this._currentRoute
+    let prevTransition = this._currentTransition
+
+    // do nothing if going to the same route.
+    // the route only changes when a transition successfully
+    // reaches activation; we don't need to do anything
+    // if an ongoing transition is aborted during validation
+    // phase.
+    if (prevTransition && path === prevRoute.path) {
+      return
+    }
+
+    // construct new route and transition context
+    let route = new Route(path, this)
+    let transition = new Transition(this, route, prevRoute)
+    this._prevTransition = prevTransition
+    this._currentTransition = transition
+
+    if (!this.app) {
+      // initial render
+      this.app = new this._appConstructor({
+        el: this._appContainer,
+        _meta: {
+          $route: route
+        }
+      })
+    }
+
+    // check global before hook
+    let beforeHooks = this._beforeEachHooks
+    let startTransition = () => {
+      transition.start(() => {
+        this._postTransition(route, state, anchor)
+      })
+    }
+
+    if (beforeHooks.length) {
+      transition.runQueue(beforeHooks, (hook, _, next) => {
+        if (transition === this._currentTransition) {
+          transition.callHook(hook, null, next, true)
+        }
+      }, startTransition)
+    } else {
+      startTransition()
+    }
+
+    if (!this._rendered && this._startCb) {
+      this._startCb.call(null)
+    }
+
+    // HACK:
+    // set rendered to true after the transition start, so
+    // that components that are acitvated synchronously know
+    // whether it is the initial render.
+    this._rendered = true
+  }
+
+  /**
+   * Set current to the new transition.
+   * This is called by the transition object when the
+   * validation of a route has succeeded.
+   *
+   * @param {Transition} transition
+   */
+
+  _onTransitionValidated (transition) {
+    // now that this one is validated, we can abort
+    // the previous transition.
+    let prevTransition = this._prevTransition
+    if (prevTransition) {
+      prevTransition.aborted = true
+    }
+    // set current route
+    let route = this._currentRoute = transition.to
+    // update route context for all children
+    if (this.app.$route !== route) {
+      this.app.$route = route
+      this._children.forEach((child) => {
+        child.$route = route
+      })
+    }
+    // call global after hook
+    if (this._afterEachHooks.length) {
+      this._afterEachHooks.forEach(hook => hook.call(null, {
+        to: transition.to,
+        from: transition.from
+      }))
+    }
+    this._currentTransition.done = true
+  }
+
+  /**
+   * Handle stuff after the transition.
+   *
+   * @param {Route} route
+   * @param {Object} [state]
+   * @param {String} [anchor]
+   */
+
+  _postTransition (route, state, anchor) {
+    // handle scroll positions
+    // saved scroll positions take priority
+    // then we check if the path has an anchor
+    let pos = state && state.pos
+    if (pos && this._saveScrollPosition) {
+      Vue.nextTick(() => {
+        window.scrollTo(pos.x, pos.y)
+      })
+    } else if (anchor) {
+      Vue.nextTick(() => {
+        let el = document.getElementById(anchor.slice(1))
+        if (el) {
+          window.scrollTo(window.scrollX, el.offsetTop)
+        }
+      })
+    }
+  }
+
+  /**
+   * Normalize named route object / string paths into
+   * a string.
+   *
+   * @param {Object|String|Number} path
+   * @return {String}
+   */
+
+  _normalizePath (path) {
+    if (typeof path === 'object') {
+      if (path.name) {
+        var params = path.params || {}
+        if (path.query) {
+          params.queryParams = path.query
+        }
+        return this._recognizer.generate(path.name, params)
+      } else if (path.path) {
+        return path.path
+      } else {
+        return ''
+      }
+    } else {
+      return path + ''
+    }
+  }
 }
+
+/**
+ * Allow directly passing components to a route
+ * definition.
+ *
+ * @param {Object} handler
+ */
+
+function guardComponent (handler) {
+  let comp = handler.component
+  if (Vue.util.isPlainObject(comp)) {
+    comp = handler.component = Vue.extend(comp)
+  }
+  /* istanbul ignore if */
+  if (typeof comp !== 'function') {
+    handler.component = null
+    warn(
+      'invalid component for route "' + handler.path + '"'
+    )
+  }
+}
+
+/* Installation */
 
 Router.installed = false
 
@@ -110,14 +545,13 @@ Router.installed = false
  * Install the necessary directives.
  */
 
-Router.install = function (Vue) {
+Router.install = function (externalVue) {
   /* istanbul ignore if */
   if (Router.installed) {
     warn('already installed.')
     return
   }
-  initAPI(Vue, Router)
-  initInternal(Vue, Router)
+  Vue = externalVue
   initMixin(Vue)
   View(Vue)
   Link(Vue)
