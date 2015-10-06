289e43f36942d43266236acbef8f40db1b8db84c
Evan You <yyx990803@gmail.com>
2015-10-06 16:45:15 -0400

    update dev script

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: './dist',
+    filename: 'vue-router.js',
+    library: 'VueRouter',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js/,
+        exclude: /node_modules/,
+        loader: 'babel?optional[]=runtime'
+      }
+    ]
+  }
+}
