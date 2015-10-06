54c89923eeab1b0a25125dadc5e8a3030d4d938c
Evan You <yyx990803@gmail.com>
2015-10-06 12:32:40 -0400

    update ci setup

---
+++ build/ci.sh
@@ -0,0 +1,14 @@
+if [ -z "$CIRCLE_PULL_REQUEST" ]
+then
+  npm run unit
+  # start sauce connect
+  cd sc-*-linux && ./bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -f ~/sc_ready &
+  # Wait for tunnel to be ready
+  while [ ! -e ~/sc_ready ]; do sleep 1; done
+  # serve example app
+  npm run serve &
+  # Run selenium tests
+  npm run e2e-sauce
+else
+  npm test
+fi
