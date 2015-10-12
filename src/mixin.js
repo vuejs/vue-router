c8ebba8592e7f35ed07dfccea090bc50b89f2609
Evan You <yyx990803@gmail.com>
2015-10-12 12:11:43 -0400

    ensure $route is available in data() (fix #177)

--- src/mixin.js
+++ src/mixin.js
@@ -2,25 +2,42 @@ export default function (Vue) {
 
   const _ = Vue.util
   const mixin = {
-    created: function () {
-      var route = this.$root.$route
+    init: function () {
+      const route = this.$root.$route
+      if (route) {
+        route.router._children.push(this)
+        if (!this.$route) {
+          _.defineReactive(this, '$route', route)
+        }
+      }
+    },
+    beforeDestroy: function () {
+      const route = this.$root.$route
+      if (route) {
+        route.router._children.$remove(this)
+      }
+    }
+  }
+
+  // pre 1.0.0-rc compat
+  if (!Vue.config.optionMergeStrategies ||
+      !Vue.config.optionMergeStrategies.init) {
+    delete mixin.init
+    const init = Vue.prototype._init
+    Vue.prototype._init = function (options) {
+      const root = options._parent || options.parent || this
+      const route = root.$route
       if (route) {
         route.router._children.push(this)
         if (!this.$route) {
           if (this._defineMeta) {
-            // 0.12 compat
             this._defineMeta('$route', route)
           } else {
             _.defineReactive(this, '$route', route)
           }
         }
       }
-    },
-    beforeDestroy: function () {
-      var route = this.$root.$route
-      if (route) {
-        route.router._children.$remove(this)
-      }
+      init.call(this, options)
     }
   }
 
