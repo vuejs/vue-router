d7cfd342107feca155d691733bbddf7b376b6516
Evan You <yyx990803@gmail.com>
2015-10-16 23:09:57 -0400

    test for waitForData + promise sugar

--- test/unit/specs/pipeline/data.js
+++ test/unit/specs/pipeline/data.js
@@ -92,6 +92,39 @@ describe('data', function () {
     })
   })
 
+  it('waitForData + promise sugar', function (done) {
+    test({
+      data: {
+        waitForData: true,
+        data: function (transition) {
+          return {
+            msg: new Promise(function (resolve) {
+              setTimeout(function () {
+                resolve(transition.to.params.msg)
+              }, wait)
+            })
+          }
+        }
+      }
+    }, function (router, calls) {
+      router.go('/data/hello')
+      assertCalls(calls, ['data.data'])
+      expect(router.app.$el.textContent).toBe('')
+      setTimeout(function () {
+        expect(router.app.$el.textContent).toBe('hello')
+        router.go('/data/reload')
+        assertCalls(calls, ['data.data', 'data.data'])
+        router.app.$nextTick(function () {
+          expect(router.app.$el.textContent).toBe('loading...')
+          setTimeout(function () {
+            expect(router.app.$el.textContent).toBe('reload')
+            done()
+          }, wait * 2)
+        })
+      }, wait * 2)
+    })
+  })
+
   it('promise error', function (done) {
     test({
       data: {
