d8a91474b3eed07fe18c12d1c8ad9c65e2d19a71
Evan You <yyx990803@gmail.com>
2015-10-17 16:30:05 -0400

    use correct loose mode option

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         test: /.js/,
         exclude: /node_modules/,
-        loader: 'babel?optional[]=runtime&loose=true'
+        loader: 'babel?optional[]=runtime&loose=all'
       }
     ]
   },
