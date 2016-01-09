bf74e6d64b04de8ddfac5aaffed5b154186607c6
Evan You <yyx990803@gmail.com>
2016-01-09 11:58:50 -0500

    expose this.$router

--- src/override.js
+++ src/override.js
@@ -1,24 +1,29 @@
 export default function (Vue) {
 
-  const _ = Vue.util
+  const {
+    extend,
+    isArray,
+    defineReactive
+  } = Vue.util
 
   // override Vue's init and destroy process to keep track of router instances
   const init = Vue.prototype._init
   Vue.prototype._init = function (options) {
     options = options || {}
     const root = options._parent || options.parent || this
+    const router = root.$router
     const route = root.$route
-    if (route) {
-      route.router._children.push(this)
-      if (!this.$route) {
-        /* istanbul ignore if */
-        if (this._defineMeta) {
-          // 0.12
-          this._defineMeta('$route', route)
-        } else {
-          // 1.0
-          _.defineReactive(this, '$route', route)
-        }
+    if (router) {
+      // expose router
+      this.$router = router
+      router._children.push(this)
+      /* istanbul ignore if */
+      if (this._defineMeta) {
+        // 0.12
+        this._defineMeta('$route', route)
+      } else {
+        // 1.0
+        defineReactive(this, '$route', route)
       }
     }
     init.call(this, options)
@@ -27,9 +32,8 @@ export default function (Vue) {
   const destroy = Vue.prototype._destroy
   Vue.prototype._destroy = function () {
     if (!this._isBeingDestroyed) {
-      const route = this.$root.$route
-      if (route) {
-        route.router._children.$remove(this)
+      if (this.$router) {
+        this.$router._children.$remove(this)
       }
       destroy.apply(this, arguments)
     }
@@ -44,14 +48,14 @@ export default function (Vue) {
       if (!childVal) return parentVal
       if (!parentVal) return childVal
       const ret = {}
-      _.extend(ret, parentVal)
+      extend(ret, parentVal)
       for (let key in childVal) {
         let a = ret[key]
         let b = childVal[key]
         // for data, activate and deactivate, we need to merge them into
         // arrays similar to lifecycle hooks.
         if (a && hooksToMergeRE.test(key)) {
-          ret[key] = (_.isArray(a) ? a : [a]).concat(b)
+          ret[key] = (isArray(a) ? a : [a]).concat(b)
         } else {
           ret[key] = b
         }
