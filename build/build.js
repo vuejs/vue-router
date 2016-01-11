312b47cf1d9f980a8a8f6f54d19f11822355973b
Evan You <yyx990803@gmail.com>
2016-01-11 16:22:25 -0500

    update build script

--- build/build.js
+++ build/build.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+var zlib = require('zlib')
 var rollup = require('rollup')
 var uglify = require('uglify-js')
 var babel = require('rollup-plugin-babel')
@@ -14,35 +15,54 @@ rollup.rollup({
   entry: 'src/index.js',
   plugins: [
     babel({
-      exclude: 'node_modules/**',
-      loose: 'all',
-      sourceMap: true
+      loose: 'all'
     })
   ]
-}).then(function (bundle) {
-  bundle.write({
-    format: 'cjs',
-    dest: 'dist/vue-router.common.js'
-  }).then(function () {
-    console.log('built: dist/vue-router.common.js')
-  })
-  return bundle.write({
+})
+.then(function (bundle) {
+  return write('dist/vue-router.js', bundle.generate({
     format: 'umd',
     banner: banner,
-    moduleName: 'VueRouter',
-    sourceMap: true,
-    dest: 'dist/vue-router.js'
-  })
-}).then(function () {
-  console.log('built: ' + 'dist/vue-router.js')
-  fs.writeFile(
-    'dist/vue-router.min.js',
-    banner + '\n' + uglify.minify('dist/vue-router.js').code,
-    function (err) {
-      if (err) throw err
-      console.log('built: ' + 'dist/vue-router.min.js')
-    }
-  )
-}).catch(function (e) {
-  console.log(e)
+    moduleName: 'VueRouter'
+  }).code)
 })
+.then(function () {
+  return write(
+    'dist/vue-router.min.js',
+    banner + '\n' + uglify.minify('dist/vue-router.js').code
+  )
+})
+.then(function () {
+  return new Promise(function (resolve, reject) {
+    fs.readFile('dist/vue-router.min.js', function (err, buf) {
+      if (err) return reject(err)
+      zlib.gzip(buf, function (err, buf) {
+        if (err) return reject(err)
+        write('dist/vue-router.min.js.gz', buf).then(resolve)
+      })
+    })
+  })
+})
+.catch(logError)
+
+function write (dest, code) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(dest, code, function (err) {
+      if (err) return reject(err)
+      console.log(blue(dest) + ' ' + getSize(code))
+      resolve()
+    })
+  })
+}
+
+function getSize (code) {
+  return (code.length / 1024).toFixed(2) + 'kb'
+}
+
+function logError (e) {
+  console.log(e)
+}
+
+function blue (str) {
+  return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
+}
