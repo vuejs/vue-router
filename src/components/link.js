918917d1be32ffcc6f043427f54faf7155be4997
Evan You <yyx990803@gmail.com>
2016-07-06 19:52:20 -0400

    router-link

--- src/components/link.js
+++ src/components/link.js
@@ -1,1 +1,27 @@
-export default {}
+export default {
+  name: 'router-link',
+  functional: true,
+  props: {
+    to: {
+      type: [String, Object],
+      required: true
+    },
+    tag: {
+      type: String,
+      default: 'a'
+    }
+  },
+  render (h, { props, children, parent }) {
+    return h(props.tag, {
+      attrs: {
+        href: props.to
+      },
+      on: {
+        click: (e) => {
+          e.preventDefault()
+          parent.$router.go(props.to)
+        }
+      }
+    }, children)
+  }
+}
