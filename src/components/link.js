4d2195af962fe67ecc7b651cb2e1e53ee4525b42
Evan You <yyx990803@gmail.com>
2016-07-22 01:43:42 -0400

    router-link active class

--- src/components/link.js
+++ src/components/link.js
@@ -1,5 +1,5 @@
 import { cleanPath, getFullPath } from '../util/path'
-import { normalizeLocation } from '../util/location'
+import { normalizeLocation, isSameLocation } from '../util/location'
 
 export default {
   name: 'router-link',
@@ -13,20 +13,26 @@ export default {
       default: 'a'
     },
     append: Boolean,
-    replace: Boolean
+    replace: Boolean,
+    activeClass: {
+      type: String,
+      default: 'router-link-active'
+    }
   },
   render (h) {
     const router = this.$router
     const current = router.history.current
     const to = normalizeLocation(this.to, current, this.append)
-    const fullPath = to.name
-      ? router.match(to).fullPath
-      : getFullPath(to)
+    const resolved = router.match(to)
+    const fullPath = resolved.fullPath
     const base = router.history.base
     const href = base ? cleanPath(base + fullPath) : fullPath
+    const classes = {}
+    classes[this.activeClass] = isSameLocation(resolved, this.$route)
 
     return h(this.tag, {
       attrs: { href },
+      class: classes,
       on: {
         click: (e) => {
           e.preventDefault()
