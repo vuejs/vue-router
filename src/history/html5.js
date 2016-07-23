2df8adcda97b1c9f1d1072460156904f2924ac0c
Evan You <yyx990803@gmail.com>
2016-07-22 23:21:36 -0400

    reload the page on pushState error to workaround Safari call limit

--- src/history/html5.js
+++ src/history/html5.js
@@ -21,14 +21,14 @@ export class HTML5History extends History {
   push (location) {
     super.push(location, resolvedLocation => {
       const url = cleanPath(this.base + resolvedLocation.fullPath)
-      window.history.pushState({}, '', url)
+      tryPushState(url)
     })
   }
 
   replace (location) {
     super.replace(location, resolvedLocation => {
       const url = cleanPath(this.base + resolvedLocation.fullPath)
-      window.history.replaceState({}, '', url)
+      tryPushState(url, true)
     })
   }
 
@@ -39,6 +39,18 @@ export class HTML5History extends History {
       path = path.slice(base.length)
     }
     return path + window.location.search + window.location.hash
+  }
+}
+
+/**
+ * try...catch the pushState call to get around Safari
+ * DOM Exception 18 where it limits to 100 pushState calls
+ */
+function tryPushState (url, replace) {
+  try {
+    window.history[replace ? 'replaceState' : 'pushState']({}, '', url)
+  } catch (e) {
+    window.location[replace ? 'assign' : 'replace'](url)
   }
 }
 
