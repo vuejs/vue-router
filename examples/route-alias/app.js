b0f37f6cbbb0b7a9e7367ebc4e80bc0cfa0d17c3
Evan You <yyx990803@gmail.com>
2016-07-24 17:37:13 -0400

    rename alias example

---
+++ examples/route-alias/app.js
@@ -0,0 +1,40 @@
+import Vue from 'vue/dist/vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Home = { template: '<div><h1>Home</h1><router-view></router-view></div>' }
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+const Baz = { template: '<div>baz</div>' }
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/home', component: Home, children: [
+      // absolute alias
+      { path: 'foo', component: Foo, alias: '/foo' },
+      // relative alias (alias to /home/bar-alias)
+      { path: 'bar', component: Bar, alias: 'bar-alias' },
+      // multiple aliases
+      { path: 'baz', component: Baz, alias: ['/baz', 'baz-alias'] }
+    ]}
+  ]
+})
+
+const app = new Vue({
+  router,
+  template: `
+    <div id="app">
+      <h1>Route Alias</h1>
+      <ul>
+        <li><router-link to="/foo">/foo (renders /home/foo)</router-link></li>
+        <li><router-link to="/home/bar-alias">/home/bar-alias (renders /home/bar)</router-link></li>
+        <li><router-link to="/baz">/baz (renders /home/baz)</router-link></li>
+        <li><router-link to="/home/baz-alias">/home/baz-alias (renders /home/baz)</router-link></li>
+      </ul>
+      <router-view class="view"></router-view>
+    </div>
+  `
+}).$mount('#app')
