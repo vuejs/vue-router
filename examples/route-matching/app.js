b69cbfdc6935e1826cf77dff8b2f480a7751efe3
Evan You <yyx990803@gmail.com>
2016-07-24 12:48:56 -0400

    add route-matching example

---
+++ examples/route-matching/app.js
@@ -0,0 +1,49 @@
+import Vue from 'vue/dist/vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// The matching uses path-to-regexp, which is the matching engine used
+// by express as well, so the same matching rules apply.
+// For detailed rules, see https://github.com/pillarjs/path-to-regexp
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/' },
+    // params are denoted with a colon ":"
+    { path: '/params/:foo/:bar' },
+    // a param can be made optional by adding "?"
+    { path: '/optional-params/:foo?'},
+    // a param can be followed by a regex pattern in parens
+    // this route will only be matched if :id is all numbers
+    { path: '/params-with-regex/:id(\\d+)' },
+    // asterisk can match anything
+    { path: '/asterisk/*' },
+    // make part of th path optional by wrapping with parens and add "?"
+    { path: '/optional-group/(foo/)?bar' }
+  ]
+})
+
+const app = new Vue({
+  router,
+  template: `
+    <div id="app">
+      <h1>Basic</h1>
+      <ul>
+        <li><router-link to="/">/</router-link></li>
+        <li><router-link to="/params/foo/bar">/params/foo/bar</router-link></li>
+        <li><router-link to="/optional-params">/optional-params</router-link></li>
+        <li><router-link to="/optional-params/foo">/optional-params/foo</router-link></li>
+        <li><router-link to="/params-with-regex/123">/params-with-regex/123</router-link></li>
+        <li><router-link to="/params-with-regex/abc">/params-with-regex/abc</router-link></li>
+        <li><router-link to="/asterisk/foo">/asterisk/foo</router-link></li>
+        <li><router-link to="/asterisk/foo/bar">/asterisk/foo/bar</router-link></li>
+        <li><router-link to="/optional-group/bar">/optional-group/bar</router-link></li>
+        <li><router-link to="/optional-group/foo/bar">/optional-group/foo/bar</router-link></li>
+      </ul>
+      <p>Route context</p>
+      <pre>{{ JSON.stringify($route, null, 2) }}</pre>
+    </div>
+  `
+}).$mount('#app')
