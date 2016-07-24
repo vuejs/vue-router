c9b703859040784728b3f9191c8e517ef685b526
Evan You <yyx990803@gmail.com>
2016-07-24 14:49:09 -0400

    add more redirect cases

--- examples/redirect/app.js
+++ examples/redirect/app.js
@@ -8,6 +8,7 @@ const Default = { template: '<div>default</div>' }
 const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
 const Baz = { template: '<div>baz</div>' }
+const WithParams = { template: '<div>{{ $route.params.id }}</div>' }
 
 const router = new VueRouter({
   mode: 'history',
@@ -25,8 +26,13 @@ const router = new VueRouter({
     { path: '/absolute-redirect', redirect: '/bar' },
     // named redirect
     { path: '/named-redirect', redirect: { name: 'baz' }},
+
+    // redirect with params
+    { path: '/with-params/:id', component: WithParams },
+    { path: '/redirect-with-params/:id', redirect: '/with-params/:id' },
+
     // catch all redirect
-    { path: '*', redirect: '/' }
+    { path: '*', redirect: '/' },
   ]
 })
 
@@ -37,8 +43,10 @@ const app = new Vue({
       <h1>Named Routes</h1>
       <ul>
         <li><router-link to="/relative-redirect">/relative-redirect (redirects to /foo)</router-link></li>
+        <li><router-link to="/relative-redirect?foo=bar">/relative-redirect?foo=bar (redirects to /foo?foo=bar)</router-link></li>
         <li><router-link to="/absolute-redirect">/absolute-redirect (redirects to /bar)</router-link></li>
         <li><router-link to="/named-redirect">/named-redirect (redirects to /baz)</router-link></li>
+        <li><router-link to="/redirect-with-params/123">/redirect-with-params/123 (redirects to /with-params/123)</router-link></li>
         <li><router-link to="/not-found">/not-found (redirects to /)</router-link></li>
       </ul>
       <router-view class="view"></router-view>
