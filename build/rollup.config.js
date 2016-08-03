e1ba83ed387650ff9d10d838f66e691ffb0b5521
Evan You <yyx990803@gmail.com>
2016-08-03 11:35:41 -0400

    remove unnecessary rollup plugins

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,7 +2,6 @@ const buble = require('rollup-plugin-buble')
 const flow = require('rollup-plugin-flow')
 const cjs = require('rollup-plugin-commonjs')
 const node = require('rollup-plugin-node-resolve')
-const replace = require('rollup-plugin-replace')
 const version = process.env.VERSION || require('../package.json').version
 
 module.exports = {
@@ -10,15 +9,7 @@ module.exports = {
   dest: 'dist/vue-router.js',
   format: 'umd',
   moduleName: 'VueRouter',
-  plugins: [
-    flow(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    node(),
-    cjs(),
-    buble()
-  ],
+  plugins: [flow(), node(), cjs(), buble()],
   banner:
 `/**
  * vue-router v${version}
