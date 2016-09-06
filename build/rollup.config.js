18d6ff99f41c715f5093f1903c59d374c8109713
Thorsten <t.luenborg@googlemail.com>
2016-09-06 18:51:50 +0200

    add replace plugin to rollup config and set production env variable in pm build script.

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,14 +2,19 @@ const buble = require('rollup-plugin-buble')
 const flow = require('rollup-plugin-flow')
 const cjs = require('rollup-plugin-commonjs')
 const node = require('rollup-plugin-node-resolve')
+const replace = require('rollup-plugin-replace')
 const version = process.env.VERSION || require('../package.json').version
+
+const replaceConfig = {
+  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+}
 
 module.exports = {
   entry: 'src/index.js',
   dest: 'dist/vue-router.js',
   format: 'umd',
   moduleName: 'VueRouter',
-  plugins: [flow(), node(), cjs(), buble()],
+  plugins: [flow(), replace(replaceConfig), node(), cjs(), buble()],
   banner:
 `/**
  * vue-router v${version}
