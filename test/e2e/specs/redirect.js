1cd198aea38d2b0f16ae0656a9c8290d2a57851b
fnlctrl <fnlctrl@gmail.com>
2016-09-08 20:45:45 +0800

    fix e2e tests

--- test/e2e/specs/redirect.js
+++ test/e2e/specs/redirect.js
@@ -3,15 +3,18 @@ module.exports = {
     browser
     .url('http://localhost:8080/redirect/')
       .waitForElementVisible('#app', 1000)
-      .assert.count('li a', 7)
+      .assert.count('li a', 10)
       // assert correct href with base
       .assert.attributeContains('li:nth-child(1) a', 'href', '/redirect/relative-redirect')
       .assert.attributeContains('li:nth-child(2) a', 'href', '/redirect/relative-redirect?foo=bar')
       .assert.attributeContains('li:nth-child(3) a', 'href', '/redirect/absolute-redirect')
       .assert.attributeContains('li:nth-child(4) a', 'href', '/redirect/dynamic-redirect')
-      .assert.attributeContains('li:nth-child(5) a', 'href', '/redirect/named-redirect')
-      .assert.attributeContains('li:nth-child(6) a', 'href', '/redirect/redirect-with-params/123')
-      .assert.attributeContains('li:nth-child(7) a', 'href', '/not-found')
+      .assert.attributeContains('li:nth-child(5) a', 'href', '/redirect/dynamic-redirect/123')
+      .assert.attributeContains('li:nth-child(6) a', 'href', '/redirect/dynamic-redirect?bar')
+      .assert.attributeContains('li:nth-child(7) a', 'href', '/redirect/dynamic-redirect#baz')
+      .assert.attributeContains('li:nth-child(8) a', 'href', '/redirect/named-redirect')
+      .assert.attributeContains('li:nth-child(9) a', 'href', '/redirect/redirect-with-params/123')
+      .assert.attributeContains('li:nth-child(10) a', 'href', '/not-found')
 
       .assert.containsText('.view', 'default')
 
