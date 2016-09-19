bbd7bea528e0f4250e04d1bcf73db1b07a004c70
Evan You <yyx990803@gmail.com>
2016-09-19 11:20:18 -0400

    fix lazy-loading e2e-tests

--- test/e2e/specs/lazy-loading.js
+++ test/e2e/specs/lazy-loading.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
     .url('http://localhost:8080/lazy-loading/')
       .waitForElementVisible('#app', 1000)
-      .assert.count('li a', 3)
+      .assert.count('li a', 4)
       .assert.containsText('.view', 'home')
 
       .click('li:nth-child(2) a')
@@ -15,10 +15,19 @@ module.exports = {
       .click('li:nth-child(1) a')
       .assert.containsText('.view', 'home')
 
+      .click('li:nth-child(4) a')
+      .assert.containsText('.view', 'This is Bar!')
+      .assert.containsText('.view h3', 'Baz')
+
       // test initial visit
     .url('http://localhost:8080/lazy-loading/foo')
       .waitForElementVisible('#app', 1000)
       .assert.containsText('.view', 'This is Foo!')
+
+    .url('http://localhost:8080/lazy-loading/bar/baz')
+      .waitForElementVisible('#app', 1000)
+      .assert.containsText('.view', 'This is Bar!')
+      .assert.containsText('.view h3', 'Baz')
       .end()
   }
 }
