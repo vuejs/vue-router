be7a13891833a0b30979a1103c198926a2a533ce
Bob Fanger <bfanger@gmail.com>
2016-11-27 15:09:55 +0100

    Let the selenium-server and chromedriver packages determine the paths
    Makes it easier to upgrade to those packages.

--- test/e2e/nightwatch.config.js
+++ test/e2e/nightwatch.config.js
@@ -1,4 +1,7 @@
 // http://nightwatchjs.org/guide#settings-file
+var seleniumServer = require('selenium-server');
+var chromedriver = require('chromedriver');
+
 module.exports = {
   'src_folders': ['test/e2e/specs'],
   'output_folder': 'test/e2e/reports',
@@ -7,11 +10,11 @@ module.exports = {
 
   'selenium': {
     'start_process': true,
-    'server_path': 'node_modules/selenium-server/lib/runner/selenium-server-standalone-2.53.1.jar',
+    'server_path': seleniumServer.path,
     'host': '127.0.0.1',
     'port': 4444,
     'cli_args': {
-      'webdriver.chrome.driver': 'node_modules/chromedriver/lib/chromedriver/chromedriver'
+      'webdriver.chrome.driver': chromedriver.path
     }
   },
 
