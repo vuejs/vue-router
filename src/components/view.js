ef130f3aaab38af1f1c8b61b8cf54a9bd6c4c2bf
Evan You <yyx990803@gmail.com>
2017-01-12 12:32:25 -0500

    <router-view>: keep previous view when tree is toggled inactive but kept-alive. (partially address vuejs/vue#4590)

--- src/components/view.js
+++ src/components/view.js
@@ -10,11 +10,14 @@ export default {
   render (h, { props, children, parent, data }) {
     data.routerView = true
 
+    const name = props.name
     const route = parent.$route
     const cache = parent._routerViewCache || (parent._routerViewCache = {})
+
+    // determine current view depth, also check to see if the tree
+    // has been toggled inactive but kept-alive.
     let depth = 0
     let inactive = false
-
     while (parent) {
       if (parent.$vnode && parent.$vnode.data.routerView) {
         depth++
@@ -24,30 +27,33 @@ export default {
       }
       parent = parent.$parent
     }
-
     data.routerViewDepth = depth
+
+    // render previous view if the tree is inactive and kept-alive
+    if (inactive) {
+      return h(cache[name], data, children)
+    }
+
     const matched = route.matched[depth]
+    // render empty node if no matched route
     if (!matched) {
+      cache[name] = null
       return h()
     }
 
-    const name = props.name
-    const component = inactive
-      ? cache[name]
-      : (cache[name] = matched.components[name])
+    const component = cache[name] = matched.components[name]
 
-    if (!inactive) {
-      const hooks = data.hook || (data.hook = {})
-      hooks.init = vnode => {
-        matched.instances[name] = vnode.child
-      }
-      hooks.prepatch = (oldVnode, vnode) => {
-        matched.instances[name] = vnode.child
-      }
-      hooks.destroy = vnode => {
-        if (matched.instances[name] === vnode.child) {
-          matched.instances[name] = undefined
-        }
+    // inject instance registration hooks
+    const hooks = data.hook || (data.hook = {})
+    hooks.init = vnode => {
+      matched.instances[name] = vnode.child
+    }
+    hooks.prepatch = (oldVnode, vnode) => {
+      matched.instances[name] = vnode.child
+    }
+    hooks.destroy = vnode => {
+      if (matched.instances[name] === vnode.child) {
+        matched.instances[name] = undefined
       }
     }
 
