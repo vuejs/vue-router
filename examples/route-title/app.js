283086af137c55b1601a24b09bf8f838366b70b8
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2017-03-08 20:24:28 +0800

    apend  children route  example

--- examples/route-title/app.js
+++ examples/route-title/app.js
@@ -5,7 +5,8 @@ Vue.use(VueRouter)
 
 const Home = { template: '<div><h2>Home</h2></div>' }
 const User = { template: '<div><h2>User</h2></div>' }
-const About = { template: '<div><h2>About</h2></div>' }
+const About = { template: '<div><h2>About</h2><router-view></router-view></div>' }
+const Child = { template: '<div><h2>Child</h2></div>' }
 
 const router = new VueRouter({
   mode: 'history',
@@ -13,7 +14,11 @@ const router = new VueRouter({
   routes: [
     { path: '/', title: 'home', component: Home },
     { path: '/user', title: 'user', component: User },
-    { path: '/about/:id', title: 'about', name: 'about', component: About }
+    { path: '/about/:id', title: 'about', name: 'about', component: About,
+      children: [
+        { path: 'child', title: 'child', component: Child }
+      ]
+    }
   ]
 })
 
@@ -27,12 +32,15 @@ new Vue({
         <li><router-link to="/">Link To /</router-link></li>
         <li><router-link to="/user">Link To /user</router-link></li>
         <li><router-link to="/about/1">Link To /about/1</router-link></li>
+        <li><router-link to="/about/1/child">Link To /about/1/child</router-link></li>
         <li><a @click="push('/')" >Push To /</a></li>
         <li><a @click="push({ path:'/user'})" >Push To /user With Location</a></li>
         <li><a @click="push({ path: '/about/1'})" >Push To /about/1 With Location</a></li>
+        <li><a @click="push('/about/1/child')" >Push To /about/1/child</a></li>
         <li><a @click="replace('/')" >Replace To /</a></li>
         <li><a @click="replace('/user')" >Replace To /user</a></li>
         <li><a @click="replace({ name: 'about',params:{id:1}})" >Replace To /about With Location&Name&Params</a></li>
+        <li><a @click="replace('/about/1/child')" >Replace To /about/1/child</a></li>
         <li><a @click="go(-1)">Go -1</a></li>
         <li><a @click="go(1)">Go 1</a></li>
       </ul>
