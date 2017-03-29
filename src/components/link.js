0eab797106257c079193f729d1b1cdc7e04a2312
Eduardo San Martin Morote <posva@users.noreply.github.com>
2017-03-29 16:32:28 +0200

    Update link.js

--- src/components/link.js
+++ src/components/link.js
@@ -91,7 +91,7 @@ function guardEvent (e) {
   // don't redirect on right click
   if (e.button !== undefined && e.button !== 0) return
   // don't redirect if `target="_blank"`
-  if (e.target && e.target.getAttribute) {
+  if (e.currentTarget && e.currentTarget.getAttribute) {
     const target = e.currentTarget.getAttribute('target')
     if (/\b_blank\b/i.test(target)) return
   }
