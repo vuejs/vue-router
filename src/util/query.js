239cdccd1ab4f0818a9739cf1e6a3f9f09dc097c
Warat Wongmaneekit <canopybanj@gmail.com>
2017-04-06 16:02:32 +0700

    Remove decode method and change parseQuery, stringifyQuery to qs based

--- src/util/query.js
+++ src/util/query.js
@@ -14,8 +14,6 @@ const encode = str => encodeURIComponent(str)
   .replace(encodeReserveRE, encodeReserveReplacer)
   .replace(commaRE, ',')
 
-const decode = decodeURIComponent
-
 export function resolveQuery (
   query: ?string,
   extraQuery: Dictionary<string> = {}
@@ -37,63 +35,12 @@ export function resolveQuery (
   }
 }
 
-function parseQuery (query: string): Dictionary<string> {
-  const res = {}
-
-  query = query.trim().replace(/^(\?|#|&)/, '')
-
-  if (!query) {
-    return res
-  }
-
-  query.split('&').forEach(param => {
-    const parts = param.replace(/\+/g, ' ').split('=')
-    const key = decode(parts.shift())
-    const val = parts.length > 0
-      ? decode(parts.join('='))
-      : null
-
-    if (res[key] === undefined) {
-      res[key] = val
-    } else if (Array.isArray(res[key])) {
-      res[key].push(val)
-    } else {
-      res[key] = [res[key], val]
-    }
-  })
-
+export function parseQuery (query: string): Dictionary<string> {
+  const res = qs.parse(query)
   return res
 }
 
 export function stringifyQuery (obj: Dictionary<string>): string {
-  // const res = obj ? Object.keys(obj).map(key => {
-  //   const val = obj[key]
-
-  //   if (val === undefined) {
-  //     return ''
-  //   }
-
-  //   if (val === null) {
-  //     return encode(key)
-  //   }
-
-  //   if (Array.isArray(val)) {
-  //     const result = []
-  //     val.slice().forEach(val2 => {
-  //       if (val2 === undefined) {
-  //         return
-  //       }
-  //       if (val2 === null) {
-  //         result.push(encode(key))
-  //       } else {
-  //         result.push(encode(key) + '=' + encode(val2))
-  //       }
-  //     })
-  //     return result.join('&')
-  //   }
-
-  //   return encode(key) + '=' + encode(val)
-  // }).filter(x => x.length > 0).join('&') : null
   const res = obj ? qs.stringify(obj, { indices: false, encoder: function (str) {
     return encode(str)
   } }) : null
