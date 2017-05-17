6fd4a5d31710dbdd581e6c9f08412171ab04bb65
Eduardo San Martin Morote <posva13@gmail.com>
2017-05-17 15:16:19 +0200

    Add ts types

--- types/router.d.ts
+++ types/router.d.ts
@@ -60,6 +60,12 @@ export interface RouterOptions {
 
 type RoutePropsFunction = (route: Route) => Object;
 
+export interface PathToRegexpOptions {
+  sensitive?: boolean;
+  strict?: boolean;
+  end?: boolean;
+}
+
 export interface RouteConfig {
   path: string;
   name?: string;
@@ -71,6 +77,8 @@ export interface RouteConfig {
   meta?: any;
   beforeEnter?: NavigationGuard;
   props?: boolean | Object | RoutePropsFunction;
+  caseSensitive?: boolean;
+  pathToRegexpOptions?: PathToRegexpOptions;
 }
 
 export interface RouteRecord {
