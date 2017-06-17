acaea3b0b1f1d9f2b96342c0b1f2b24381d0df17
Evan You <yyx990803@gmail.com>
2017-06-17 15:43:31 +0800

    fix base url origin stripping

--- src/history/base.js
+++ src/history/base.js
@@ -193,6 +193,8 @@ function normalizeBase (base: ?string): string {
       // respect <base> tag
       const baseEl = document.querySelector('base')
       base = (baseEl && baseEl.getAttribute('href')) || '/'
+      // strip full URL origin
+      base = base.replace(/^https?:\/\/[^\/]+/, '')
     } else {
       base = '/'
     }
