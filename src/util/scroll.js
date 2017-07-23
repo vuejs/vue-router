53d0f81c6540aead0ee70252643a25fe7ecd2fab
Eduardo San Martin Morote <posva@users.noreply.github.com>
2017-07-23 11:32:50 +0200

    Update scroll.js

--- src/util/scroll.js
+++ src/util/scroll.js
@@ -7,6 +7,7 @@ import { getStateKey, setStateKey } from './push-state'
 const positionStore = Object.create(null)
 
 export function setupScroll () {
+  // Fix for #1585 for Firefox
   window.history.replaceState({ key: getStateKey() }, '')
   window.addEventListener('popstate', e => {
     saveScrollPosition()
