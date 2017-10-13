eea2d82e76679f864f11d405dcb315792ccab424
Evan You <yyx990803@gmail.com>
2017-10-13 12:22:10 -0400

    run e2e in phantom by default

--- test/e2e/runner.js
+++ test/e2e/runner.js
@@ -10,7 +10,7 @@ if (args.indexOf('--config') === -1) {
   args = args.concat(['--config', 'test/e2e/nightwatch.config.js'])
 }
 if (args.indexOf('--env') === -1) {
-  args = args.concat(['--env', process.env.CIRCLECI ? 'phantomjs' : 'chrome'])
+  args = args.concat(['--env', 'phantomjs'])
 }
 var i = args.indexOf('--test')
 if (i > -1) {
