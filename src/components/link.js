29850e1ec25ccd9dcd65c07ba15b2ec8657ca9a7
Samuli <samuli@custobar.com>
2018-02-20 21:48:55 +0200

    Adding forgotten router-link prop linkExactPathActiveClass

--- src/components/link.js
+++ src/components/link.js
@@ -24,6 +24,7 @@ export default {
     replace: Boolean,
     activeClass: String,
     exactActiveClass: String,
+    exactPathActiveClass: String,
     event: {
       type: eventTypes,
       default: 'click'
