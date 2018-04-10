9b83eb1915c76d381661eef0ba8e0b9fdf53accc
Eduardo San Martin Morote <posva13@gmail.com>
2018-04-10 14:32:04 +0200

    test(link): add tests for nested params and custom props

--- examples/router-link/app.js
+++ examples/router-link/app.js
@@ -3,13 +3,27 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const Home = { template: '<div><h2>Home</h2></div>' }
+const Home = { template: '<div><h2>{{ $route.fullPath }}</h2></div>' }
+const Sub = { template: '<div><h2>Nested View</h2><router-view/></div>' }
+const SubNested = { template: '<div><h2>Sub</h2><pre>{{ $route.params.id }}</pre></div>' }
+
+const CustomLink = {
+  props: ['disabled', 'n'],
+  template: '<button :disabled="disabled">{{ n }} <slot/></button>'
+}
+
+Vue.component('CustomLink', CustomLink)
 
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: [
-    { path: '/', component: Home }
+    { path: '/', component: Home },
+    { path: '/other', component: Home },
+    { path: '/sub/:id', component: Sub, children: [
+      { path: 'nested1', component: SubNested, name: 'sub1' },
+      { path: 'nested2', component: SubNested, name: 'sub2' }
+    ]}
   ]
 })
 
@@ -19,7 +33,14 @@ new Vue({
     <div id="app">
       <h1>Router Link lol</h1>
       <ul>
-        <li><router-link to="/" class="custom-class">/</router-link></li>
+        <li><router-link to="/" class="custom-class" :class="{ otherClass: true }">/</router-link></li>
+        <li><router-link is="CustomLink" disabled n="N" to="/other">CustomLink</router-link></li>
+        <li><router-link to="/" style="color: crimson" :style="{ fontSize: '8px' }">/</router-link></li>
+        <li><router-link to="/other">/other</router-link></li>
+        <li><router-link to="/sub/1">/sub/1</router-link></li>
+        <li><router-link to="/sub/2">/sub/2</router-link></li>
+        <li><router-link :to="{ name: 'sub1' }">/sub/:id/nested1</router-link></li>
+        <li><router-link :to="{ name: 'sub2' }">/sub/:id/nested2</router-link></li>
       </ul>
       <router-view class="view"></router-view>
     </div>
--- test/e2e/specs/router-link.js
+++ test/e2e/specs/router-link.js
@@ -7,7 +7,22 @@ module.exports = {
       .waitForElementVisible('#app', 1000)
       // assert correct href with base
       .assert.cssClassPresent('li:nth-child(1) a', 'custom-class')
-      .assert.containsText('.view', 'Home')
+      .assert.cssClassPresent('li:nth-child(1) a', 'otherClass')
+      .assert.cssClassPresent('li:nth-child(1) a', 'router-link-active')
+
+      .assert.containsText('li:nth-child(2) button', 'N CustomLink')
+
+      .assert.attributeContains('li:nth-child(3) a', 'style', 'color:')
+      .assert.attributeContains('li:nth-child(3) a', 'style', 'font-size: 8px')
+
+      .click('li:nth-child(5) a')
+      .assert.attributeContains('li:nth-child(7) a', 'href', '/router-link/sub/1/nested1')
+      .assert.attributeContains('li:nth-child(8) a', 'href', '/router-link/sub/1/nested2')
+      .click('li:nth-child(6) a')
+      .assert.attributeContains('li:nth-child(7) a', 'href', '/router-link/sub/2/nested1')
+      .assert.attributeContains('li:nth-child(8) a', 'href', '/router-link/sub/2/nested2')
+
+      browser.expect.element('li:nth-child(2) button').to.have.attribute('disabled')
 
     browser.end()
 
