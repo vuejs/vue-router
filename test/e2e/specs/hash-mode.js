c79a7243cd728a9d087535a37563c15e8e0382a8
Emanuel Mutschlechner <emanuelmutschlechner@users.noreply.github.com>
2018-06-19 10:44:58 +0200

    Add hash mode deeplinking test

--- test/e2e/specs/hash-mode.js
+++ test/e2e/specs/hash-mode.js
@@ -2,7 +2,7 @@ module.exports = {
   'Hash mode': function (browser) {
     browser
       .url('http://localhost:8080/hash-mode/')
-      .waitForElementVisible('#app', 1000)
+      .waitForElementVisible('#app', 2000)
       .assert.count('li', 5)
       .assert.count('li a', 4)
       .assert.attributeContains('li:nth-child(1) a', 'href', '/hash-mode/#/')
@@ -27,13 +27,25 @@ module.exports = {
       .assert.urlEquals('http://localhost:8080/hash-mode/#/bar')
       .assert.containsText('.view', 'bar')
 
-    // check initial visit
+      // check initial visit
       .url('http://localhost:8080/hash-mode/#/foo')
       .waitForElementVisible('#app', 1000)
       .assert.containsText('.view', 'foo')
       .url('http://localhost:8080/hash-mode/#/%C3%A9')
       .waitForElementVisible('#app', 1000)
       .assert.containsText('.view', 'unicode')
+      .end()
+
+      // check hash placed correctly
+      .url('http://localhost:8080/hash-mode/foo?page=123')
+      .waitForElementVisible('#app', 1000)
+      .assert.urlEquals('http://localhost:8080/hash-mode/#/foo?page=123')
+      .assert.count('li', 4)
+      .assert.count('li a', 3)
+      .assert.attributeContains('li:nth-child(1) a', 'href', '/hash-mode/#/')
+      .assert.attributeContains('li:nth-child(2) a', 'href', '/hash-mode/#/foo')
+      .assert.attributeContains('li:nth-child(3) a', 'href', '/hash-mode/#/bar')
+      .assert.containsText('.view', 'foo')
       .end()
   }
 }
