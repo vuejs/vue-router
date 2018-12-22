bbfb889fad37ed3de26ecf2c5962ac6a146b7a95
Xin Du (Clark) <clark.duxin@gmail.com>
2018-12-22 19:19:06 +0000

    fix: removes warning resolving asterisk routes
    Closes #2505

--- src/util/params.js
+++ src/util/params.js
@@ -17,11 +17,18 @@ export function fillParams (
     const filler =
       regexpCompileCache[path] ||
       (regexpCompileCache[path] = Regexp.compile(path))
+    if (params && params.pathMatch) {
+      params[0] = params.pathMatch
+    }
     return filler(params || {}, { pretty: true })
   } catch (e) {
     if (process.env.NODE_ENV !== 'production') {
       warn(false, `missing param for ${routeMsg}: ${e.message}`)
     }
     return ''
+  } finally {
+    if (params && params[0]) {
+      delete params[0]
+    }
   }
 }
