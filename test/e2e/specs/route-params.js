9fe3cefb73ce0d71fbea3091dd47a37e237edd11
zrh122 <1229550935@qq.com>
2019-06-05 14:29:53 +0800

    fix(normalizeLocation): add a copy for params with named locations

---
+++ test/e2e/specs/route-params.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'route-params': function (browser) {
+    browser
+      .url('http://localhost:8080/route-params/')
+      .waitForElementVisible('#app', 1000)
+      .assert.count('li a', 2)
+
+      // https://github.com/vuejs/vue-router/issues/2800
+      .click('li:nth-child(1) a')
+      .assert.urlEquals('http://localhost:8080/route-params/items/1/logs')
+      .click('.child-link')
+      .assert.urlEquals('http://localhost:8080/route-params/items/1/logs/info')
+      .assert.containsText('.log', 'id: 1, type: info')
+
+      .click('li:nth-child(2) a')
+      .assert.urlEquals('http://localhost:8080/route-params/items/2/logs')
+      .click('.child-link')
+      .assert.urlEquals('http://localhost:8080/route-params/items/2/logs/info')
+      .assert.containsText('.log', 'id: 2, type: info')
+
+      .end()
+  }
+}
