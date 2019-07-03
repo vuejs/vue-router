e82fbb0926a6c9c673f8cb7d2dd3933db96077a2
Eduardo San Martin Morote <posva13@gmail.com>
2019-07-03 16:25:24 +0200

    chore(release): add v before version

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,11 @@ set -e
 echo "Enter release version: "
 read VERSION
 
-read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+read -p "Releasing v$VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Releasing $VERSION ..."
+  echo "Releasing v$VERSION ..."
   npm test
 
   # commit
