0ec4713a1d465928fc187a1dc192553027f2ee95
Eduardo San Martin Morote <posva13@gmail.com>
2019-07-05 15:51:21 +0200

    refactor: suppress usage of setPrototypeOf

--- src/history/errors.js
+++ src/history/errors.js
@@ -1,6 +1,6 @@
 export class NavigationDuplicated extends Error {
   constructor () {
     super('Navigating to current location is not allowed')
-    Object.setPrototypeOf(this, new.target.prototype)
+    this.name = this.constructor.name
   }
 }
