fba79dbfe4196954373e870539cf5a8dec297409
Eduardo San Martin Morote <posva13@gmail.com>
2019-07-24 13:38:15 +0200

    test: add test for router-link v-slot

--- test/e2e/specs/basic.js
+++ test/e2e/specs/basic.js
@@ -9,14 +9,17 @@ module.exports = {
     browser
       .url('http://localhost:8080/basic/')
       .waitForElementVisible('#app', 1000)
-      .assert.count('li', 7)
-      .assert.count('li a', 7)
+      .assert.count('li', 8)
+      .assert.count('li a', 8)
       // assert correct href with base
       .assert.attributeContains('li:nth-child(1) a', 'href', '/basic/')
       .assert.attributeContains('li:nth-child(2) a', 'href', '/basic/foo')
       .assert.attributeContains('li:nth-child(3) a', 'href', '/basic/bar')
       .assert.attributeContains('li:nth-child(4) a', 'href', '/basic/bar')
       .assert.attributeContains('li:nth-child(5) a', 'href', '/basic/%C3%A9')
+      .assert.attributeContains('li:nth-child(6) a', 'href', '/basic/%C3%A9?t=%25%C3%B1')
+      .assert.attributeContains('li:nth-child(7) a', 'href', '/basic/%C3%A9#%25%C3%B1')
+      .assert.attributeContains('li:nth-child(8) a', 'href', '/basic/foo')
       .assert.containsText('.view', 'home')
 
       .click('li:nth-child(2) a')
@@ -54,6 +57,18 @@ module.exports = {
       .assert.containsText('#counter', '1')
       .click('#navigate-btn')
       .assert.containsText('#counter', '2')
+
+      // scoped slot
+      .assert.containsText('li:nth-child(8) a', '/foo (with v-slot)')
+      .click('li:nth-child(8) a')
+      .assert.urlEquals('http://localhost:8080/basic/foo')
+      .assert.containsText('.view', 'foo')
+      .click('li:nth-child(2) a')
+      .assert.urlEquals('http://localhost:8080/basic/foo')
+      .assert.containsText('.view', 'foo')
+      .assert.cssClassPresent('li:nth-child(8)', 'active')
+      .assert.cssClassPresent('li:nth-child(8)', 'exact-active')
+      .assert.attributeEquals('li:nth-child(8) a', 'class', '')
 
       .end()
   }
