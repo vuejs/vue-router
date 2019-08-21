5ef5d73fc4118faadbcdaefecbfb50c71ce129a0
Eduardo San Martin Morote <posva13@gmail.com>
2019-08-21 18:01:01 +0200

    feat(errors): add stack trace to NavigationDuplicated
    Closes #2881

--- src/history/errors.js
+++ src/history/errors.js
@@ -1,7 +1,20 @@
 export class NavigationDuplicated extends Error {
-  constructor () {
-    super('Navigating to current location is not allowed')
+  constructor (normalizedLocation) {
+    super()
     this.name = this._name = 'NavigationDuplicated'
+    // passing the message to super() doesn't seem to work in the transpiled version
+    this.message = `Navigating to current location ("${
+      normalizedLocation.fullPath
+    }") is not allowed`
+    // add a stack property so services like Sentry can correctly display it
+    Object.defineProperty(this, 'stack', {
+      value: new Error().stack,
+      writable: true,
+      configurable: true
+    })
+    // we could also have used
+    // Error.captureStackTrace(this, this.constructor)
+    // but it only exists on node and chrome
   }
 }
 
