8bc2319a5d1fb0eb5392c523b977089717808194
Calvin Liang <me@calvin.sh>
2020-05-14 15:03:01 -0700

    test(scroll-behavior): add tests for scrollBehavior on load

--- test/e2e/specs/scroll-behavior.js
+++ test/e2e/specs/scroll-behavior.js
@@ -127,6 +127,44 @@ module.exports = {
         null,
         'scroll to anchor that starts with number'
       )
+
+      .url('http://localhost:8080/scroll-behavior/bar#anchor')
+      .execute(function () {
+        location.reload(true)
+      })
+      .waitForElementVisible('#app', 1000)
+      .assert.evaluate(
+        function () {
+          return document.getElementById('anchor').getBoundingClientRect().top < 1
+        },
+        null,
+        'scroll to anchor on load'
+      )
+      .url('http://localhost:8080/scroll-behavior/bar#anchor2')
+      .execute(function () {
+        location.reload(true)
+      })
+      .waitForElementVisible('#app', 1000)
+      .assert.evaluate(
+        function () {
+          return document.getElementById('anchor2').getBoundingClientRect().top < 101
+        },
+        null,
+        'scroll to anchor with offset on load'
+      )
+      .url('http://localhost:8080/scroll-behavior/bar#1number')
+      .execute(function () {
+        location.reload(true)
+      })
+      .waitForElementVisible('#app', 1000)
+      .assert.evaluate(
+        function () {
+          return document.getElementById('1number').getBoundingClientRect().top < 1
+        },
+        null,
+        'scroll to anchor that starts with number on load'
+      )
+
       .end()
   }
 }
