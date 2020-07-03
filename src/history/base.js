6ad2353402e5b8d1e93ccb113e70529a5d22ec44
Raul Cabello <raul.cabellomartin@novatec-gmbh.de>
2020-07-03 20:38:46 +0200

    feat(errors): capture errors thrown in redirect callback in onError
    remove unnecessary if
    
    Resolves #3201

--- src/history/base.js
+++ src/history/base.js
@@ -88,47 +88,45 @@ export class History {
       // https://github.com/vuejs/vue-router/issues/3201
       throw e
     }
-    if (route) {
-      this.confirmTransition(
-        route,
-        () => {
-          const prev = this.current
-          this.updateRoute(route)
-          onComplete && onComplete(route)
-          this.ensureURL()
-          this.router.afterHooks.forEach(hook => {
-            hook && hook(route, prev)
-          })
+    this.confirmTransition(
+      route,
+      () => {
+        const prev = this.current
+        this.updateRoute(route)
+        onComplete && onComplete(route)
+        this.ensureURL()
+        this.router.afterHooks.forEach(hook => {
+          hook && hook(route, prev)
+        })
 
-          // fire ready cbs once
-          if (!this.ready) {
-            this.ready = true
+        // fire ready cbs once
+        if (!this.ready) {
+          this.ready = true
+          this.readyCbs.forEach(cb => {
+            cb(route)
+          })
+        }
+      },
+      err => {
+        if (onAbort) {
+          onAbort(err)
+        }
+        if (err && !this.ready) {
+          this.ready = true
+          // Initial redirection should still trigger the onReady onSuccess
+          // https://github.com/vuejs/vue-router/issues/3225
+          if (!isRouterError(err, NavigationFailureType.redirected)) {
+            this.readyErrorCbs.forEach(cb => {
+              cb(err)
+            })
+          } else {
             this.readyCbs.forEach(cb => {
               cb(route)
             })
           }
-        },
-        err => {
-          if (onAbort) {
-            onAbort(err)
-          }
-          if (err && !this.ready) {
-            this.ready = true
-            // Initial redirection should still trigger the onReady onSuccess
-            // https://github.com/vuejs/vue-router/issues/3225
-            if (!isRouterError(err, NavigationFailureType.redirected)) {
-              this.readyErrorCbs.forEach(cb => {
-                cb(err)
-              })
-            } else {
-              this.readyCbs.forEach(cb => {
-                cb(route)
-              })
-            }
-          }
         }
-      )
-    }
+      }
+    )
   }
 
   confirmTransition (route: Route, onComplete: Function, onAbort?: Function) {
