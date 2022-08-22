0be95c5884b781568760e9f71aa3e609cc2f1640
Eduardo San Martin Morote <posva13@gmail.com>
2022-08-22 15:01:29 +0200

    chore: add missing module for ts

--- types/composables.d.ts
+++ types/composables.d.ts
@@ -1,22 +1,27 @@
-import { Route, VueRouter, NavigationGuard } from './router'
+declare module 'vue-router/composables' {
+  import type { Route, NavigationGuard, default as VueRouter } from 'vue-router'
 
-/**
- * Returns the current route location. Equivalent to using `$route` inside templates.
- */
-export declare function useRoute(): Route
-/**
- * Returns the router instance. Equivalent to using `$router` inside templates.
- */
-export declare function useRouter(): VueRouter
-/**
- * Add a navigation guard that triggers whenever the current location is about to be updated. Similar to beforeRouteUpdate but can be used in any component. The guard is removed when the component is unmounted.
- *
- * @param updateGuard NavigationGuard
- */
-export declare function onBeforeRouteUpdate(updateGuard: NavigationGuard): void
-/**
- * Add a navigation guard that triggers whenever the component for the current location is about to be left. Similar to beforeRouteLeave but can be used in any component. The guard is removed when the component is unmounted.
- *
- * @param leaveGuard NavigationGuard
- */
-export declare function onBeforeRouteLeave(leaveGuard: NavigationGuard): void
+  /**
+   * Returns the current route location. Equivalent to using `$route` inside templates.
+   */
+  export function useRoute(): Route
+
+  /**
+   * Returns the router instance. Equivalent to using `$router` inside templates.
+   */
+  export function useRouter(): VueRouter
+
+  /**
+   * Add a navigation guard that triggers whenever the current location is about to be updated. Similar to beforeRouteUpdate but can be used in any component. The guard is removed when the component is unmounted.
+   *
+   * @param updateGuard NavigationGuard
+   */
+  export function onBeforeRouteUpdate(updateGuard: NavigationGuard): void
+
+  /**
+   * Add a navigation guard that triggers whenever the component for the current location is about to be left. Similar to beforeRouteLeave but can be used in any component. The guard is removed when the component is unmounted.
+   *
+   * @param leaveGuard NavigationGuard
+   */
+  export function onBeforeRouteLeave(leaveGuard: NavigationGuard): void
+}
