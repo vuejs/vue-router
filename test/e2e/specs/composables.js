f2eacabc82f7774de2093ddece5ffc041cee9504
Eduardo San Martin Morote <posva13@gmail.com>
2022-08-22 14:34:38 +0200

    test: fixes

--- test/e2e/specs/composables.js
+++ test/e2e/specs/composables.js
@@ -30,14 +30,17 @@ module.exports = {
     browser
       .url('http://localhost:8080/composables/')
       .waitForElementVisible('#app', 1000)
-      .assert.containsText('.view', 'Home')
 
+      .assert.containsText('.view', 'Home')
       .assert.containsText('#nested-active', '/composables/nested: false, false')
       .click('li:nth-child(3) a')
+      .assert.containsText('.view', 'NestedEmpty')
       .assert.containsText('#nested-active', '/composables/nested: true, true')
       .click('li:nth-child(4) a')
+      .assert.containsText('.view', 'NestedA')
       .assert.containsText('#nested-active', '/composables/nested: true, false')
       .click('#nested-active')
+      .assert.containsText('.view', 'NestedEmpty')
       .assert.containsText('#nested-active', '/composables/nested: true, true')
 
       .end()
