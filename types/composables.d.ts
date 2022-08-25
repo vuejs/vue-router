0cf54de782a0b05692bbe78a7181495b6a35b8d9
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2022-08-24 23:18:26 -0700

    feat(types): add composables.d.ts in root (#3784)

--- types/composables.d.ts
+++ types/composables.d.ts
@@ -1,53 +1,51 @@
-declare module 'vue-router/composables' {
-  import type { ComputedRef, Ref } from 'vue'
-  import type { Route, NavigationGuard, default as VueRouter } from 'vue-router'
+import type { ComputedRef, Ref } from 'vue'
+import type { Route, NavigationGuard, default as VueRouter } from './index'
 
+/**
+ * Returns the current route location. Equivalent to using `$route` inside templates.
+ */
+export function useRoute(): Route
+
+/**
+ * Returns the router instance. Equivalent to using `$router` inside templates.
+ */
+export function useRouter(): VueRouter
+
+/**
+ * Add a navigation guard that triggers whenever the current location is about to be updated. Similar to beforeRouteUpdate but can be used in any component. The guard is removed when the component is unmounted.
+ *
+ * @param updateGuard
+ */
+export function onBeforeRouteUpdate(updateGuard: NavigationGuard): void
+
+/**
+ * Add a navigation guard that triggers whenever the component for the current location is about to be left. Similar to beforeRouteLeave but can be used in any component. The guard is removed when the component is unmounted.
+ *
+ * @param leaveGuard
+ */
+export function onBeforeRouteLeave(leaveGuard: NavigationGuard): void
+
+export interface RouterLinkOptions {
   /**
-   * Returns the current route location. Equivalent to using `$route` inside templates.
+   * Route Location the link should navigate to when clicked on.
    */
-  export function useRoute(): Route
-
+  to: Route | Ref<Route>
   /**
-   * Returns the router instance. Equivalent to using `$router` inside templates.
+   * Calls `router.replace` instead of `router.push`.
    */
-  export function useRouter(): VueRouter
+  replace?: boolean
+}
 
-  /**
-   * Add a navigation guard that triggers whenever the current location is about to be updated. Similar to beforeRouteUpdate but can be used in any component. The guard is removed when the component is unmounted.
-   *
-   * @param updateGuard
-   */
-  export function onBeforeRouteUpdate(updateGuard: NavigationGuard): void
-
-  /**
-   * Add a navigation guard that triggers whenever the component for the current location is about to be left. Similar to beforeRouteLeave but can be used in any component. The guard is removed when the component is unmounted.
-   *
-   * @param leaveGuard
-   */
-  export function onBeforeRouteLeave(leaveGuard: NavigationGuard): void
-
-  export interface RouterLinkOptions {
-    /**
-     * Route Location the link should navigate to when clicked on.
-     */
-    to: Route | Ref<Route>
-    /**
-     * Calls `router.replace` instead of `router.push`.
-     */
-    replace?: boolean
-  }
-
-  /**
-   * Vue Router 4 `useLink()` function. Note the active behavior is different from Vue Router 3 as highlighted in the
-   * migration guide (https://router.vuejs.org/guide/migration/index.html#removal-of-the-exact-prop-in-router-link)
-   *
-   * @param props - object containing a `to` property with the location
-   */
-  export function useLink(props: RouterLinkOptions): {
-    route: ComputedRef<Route>,
-    isActive: ComputedRef<boolean>,
-    isExactActive: ComputedRef<boolean>,
-    href: ComputedRef<string>,
-    navigate: () => Promise<void>,
-  }
+/**
+ * Vue Router 4 `useLink()` function. Note the active behavior is different from Vue Router 3 as highlighted in the
+ * migration guide (https://router.vuejs.org/guide/migration/index.html#removal-of-the-exact-prop-in-router-link)
+ *
+ * @param props - object containing a `to` property with the location
+ */
+export function useLink({ to, replace }: RouterLinkOptions): {
+  route: ComputedRef<Route>,
+  isActive: ComputedRef<boolean>,
+  isExactActive: ComputedRef<boolean>,
+  href: ComputedRef<string>,
+  navigate: () => Promise<void>,
 }
